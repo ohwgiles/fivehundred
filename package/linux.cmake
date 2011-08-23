597fd05a4943f0e2c043341b157fe21081d2dbea
Oliver Giles <ohw.giles@gmail.com>
2011-08-23 08:11:31 +0000

    Apparently if(LINUX) doesn't work...

--- package/linux.cmake
+++ package/linux.cmake
@@ -13,7 +13,7 @@ if(CMAKE_SIZEOF_VOID_P EQUAL 8)
 	endif()
  
     set(CPACK_DEBIAN_PACKAGE_NAME "fivehundred")
-    set(CPACK_DEBIAN_PACKAGE_DEPENDS "libstdc++6 (>= 4.5), libqt4-core (>= 4.5), liblua5.1-0-dev")
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "libstdc++6 (>= 4.5), libqt4-core (>= 4.5), liblua5.1-0")
     set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Oliver Giles")
 	set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Five Hundred is a card game for four players similar to Euchre or Bridge. This cross-platform Qt-based implementation includes both a graphical mode and a non-interactive mode and Lua scripting engine to develop AI players")
     set(CPACK_GENERATOR "DEB")
