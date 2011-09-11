49382d82eb76fdfa13746e36ed06b710abc325e1
Oliver Giles <ohw.giles@gmail.com>
2011-09-11 01:01:15 +0000

    Add man page, fix all lintian errors

--- package/debian.cmake
+++ package/debian.cmake
@@ -3,7 +3,11 @@ install(TARGETS fivehundred RUNTIME DESTINATION games)
 install(DIRECTORY gfx DESTINATION share/fivehundred PATTERN ".svn" EXCLUDE)
 install(DIRECTORY scripts DESTINATION share/fivehundred PATTERN ".svn" EXCLUDE)
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/fivehundred.desktop DESTINATION share/applications)
-install(FILES ${CMAKE_SOURCE_DIR}/README ${CMAKE_SOURCE_DIR}/debian/copyright DESTINATION share/doc/fivehundred/)
+install(FILES package/fivehundred.6.gz DESTINATION share/man/man6)
+
+# Draconian debian packaging requirements
+install(FILES README package/copyright DESTINATION share/doc/fivehundred/)
+install(FILES package/changelog.gz DESTINATION share/doc/fivehundred)
 	
 if(CMAKE_SIZEOF_VOID_P EQUAL 8)
 	set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64")
@@ -18,8 +22,8 @@ set(CPACK_DEBIAN_PACKAGE_SECTION "games")
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "libstdc++6 (>= 4.5), libqt4-core (>= 4.5), liblua5.1-0, libc6 (>= 2.12)")
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Oliver Giles <ohwgiles@users.sourceforge.net>")
 set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "http://fivehundred.sf.net")
-set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Five Hundred is a card game for four players similar to Euchre or Bridge.")
-set(CPACK_DEBIAN_PACKAGE_EXTENDED_DESCRIPTION "Five Hundred is a card game for four players similar to Euchre or Bridge.")
+execute_process(COMMAND cat ${CMAKE_SOURCE_DIR}/DESCRIPTION OUTPUT_VARIABLE CPACK_DEBIAN_PACKAGE_DESCRIPTION)
 set(CPACK_DEBIAN_PACKAGE_DEBUG ON)
 set(CPACK_GENERATOR "DEB")
+
 
